7247c0d049b99203d881c94af399cf6ca5075d05
Brandon Lyons <brandonlyons@Brandons-MacBook-Pro.local>
2020-10-20 19:13:28 -0400

    Remove ping function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
 console.log("hello world");
-const MailchimpApiKey = process.env.MAILCHIMP_API;
-const MailchimpDc = process.env.MAILCHIMP_DC;
-
-async function ping() {
-  const resp = await fetch(
-    `https://${MailchimpDc}.api.mailchimp.com/3.0/ping`,
-    {
-      mode: "no-cors",
-      headers: {
-        authorization: btoa(`user:${MailchimpApiKey}`),
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    }
-  );
-}
-
-ping();
