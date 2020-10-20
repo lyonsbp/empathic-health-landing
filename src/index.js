3c85e2af20d46e06338ca31877e0d7c374652aa5
Brandon Lyons <brandonlyons@Brandons-MacBook-Pro.local>
2020-10-20 18:49:28 -0400

    Add ping function for Mailchimp api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+console.log("hello world");
+const MailchimpApiKey = process.env.MAILCHIMP_API;
+const MailchimpDc = process.env.MAILCHIMP_DC;
+
+async function ping() {
+  const resp = await fetch(
+    `https://${MailchimpDc}.api.mailchimp.com/3.0/ping`,
+    {
+      mode: "no-cors",
+      headers: {
+        authorization: btoa(`user:${MailchimpApiKey}`),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
+
+ping();
