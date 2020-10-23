62c0659aace008b81465ab8cc1ef2c5cb4f424f6
Brandon Lyons <lyonsbp@umich.edu>
2020-10-23 14:54:48 -0400

    Add success alert

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-document
-  .querySelector("#sign-up-form")
-  .addEventListener("submit", async (evt) => {
-    evt.preventDefault();
-    const email = document.querySelector("#email-input").value;
-    const firstName = document.querySelector("#fname-input").value;
-    const lastName = document.querySelector("#lname-input").value;
+const alertBox = document.querySelector("#success-alert");
+const signUpForm = document.querySelector("#sign-up-form");
 
-    try {
-      const resp = await fetch(
-        `/.netlify/functions/add-contact?email=${email}&firstName=${firstName}&lastName=${lastName}`
-      );
-      const data = await resp.json();
-      console.log(data);
-      alert(data.msg);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+signUpForm.addEventListener("submit", async (evt) => {
+  evt.preventDefault();
+  const email = document.querySelector("#email-input").value;
+  const firstName = document.querySelector("#fname-input").value;
+  const lastName = document.querySelector("#lname-input").value;
+
+  try {
+    const resp = await fetch(
+      `/.netlify/functions/add-contact?email=${email}&firstName=${firstName}&lastName=${lastName}`
+    );
+    const data = await resp.json();
+    console.log(data);
+    alertBox.classList.remove("hidden");
+    /* setTimeout(() => {
+      alertBox.classList.add("hidden");
+    }, 3000); */
+  } catch (err) {
+    console.log(err);
+  }
+});
