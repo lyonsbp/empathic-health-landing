be7b8644e7105c0507fea785513a337ba33c4ac8
Brandon Lyons <lyonsbp@umich.edu>
2020-10-22 23:23:42 -0400

    Add contact working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+
+document
+  .querySelector("#sign-up-form")
+  .addEventListener("submit", async (evt) => {
+    evt.preventDefault();
+    const email = document.querySelector("#email-input").value;
+
+    try {
+      const resp = await fetch(
+        `/.netlify/functions/add-contact?email=${email}`
+      );
+      const data = await resp.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
