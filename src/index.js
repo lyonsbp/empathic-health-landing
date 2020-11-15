9c8b6f403addec036edf3100c8992818f20e26a3
Brandon Lyons <lyonsbp@umich.edu>
2020-11-15 16:30:05 -0500

    Reduce icon size?

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
 
 const howSelectBox = document.querySelector("#how-select");
 const learnMoreBtn = document.querySelector("#learn-more-btn");
